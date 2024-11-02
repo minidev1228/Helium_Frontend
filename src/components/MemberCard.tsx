2965e57ac59bc248a3506d6a42be051583b2bdde
minidev1228 <mightybluedev@gmail.com>
2024-11-03 07:59:50 +0900

    Change fonts

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const MemberCard: React.FC<Props> = ({title, img, name, content}) =>{
     return (
-        <div className="bg-yellow h-80 flex flex-col items-center float-right rounded-lg text-black pt-5 p-5 mr-10" style={{width:"250px"}}>
+        <div className="bg-yellow mt-5 ml-5 h-80 flex flex-col items-center float-left rounded-lg text-black pt-5 p-5 mr-10" style={{width:"250px"}}>
             <p className="mb-5 text-center text-xs">{title}</p>
             <img className="mb-5 size-20" src={img} alt="" />
             <p className="mb-5  text-center">{name}</p>
